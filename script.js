6d8acb6989dc0d662dd9f584fb094313c438349c
Oksana(KsyFate) <oprokopieva382@gmail.com>
2022-12-05 20:08:10 -0600

    Simple multiplication

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 //Reduce but Grow
-function grow(x) {
-  let sum = 1;
-  for (i = 0; i < x.length; i++) {
-    sum = sum * x[i];
-  }
-  return sum;
+// function grow(x) {
+//   let sum = 1;
+//   for (i = 0; i < x.length; i++) {
+//     sum = sum * x[i];
+//   }
+//   return sum;
+// }
+// //Simple multiplication
+// function simpleMultiplication(number) {
+//   if (number % 2 == 0) {
+//     return number * 8;
+//   } else {
+//     return number * 9;
+//   }
+// }
+//more simple way
+function simpleMultiplication(n) {
+  return n % 2 == 0 ? n * 8 : n * 9;
 }
