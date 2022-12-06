d3b7b1c964395704427ac0db161eeb7f5e0a0c20
Oksana(KsyFate) <oprokopieva382@gmail.com>
2022-12-05 22:27:21 -0600

    A Needle in the Haystack test

--- script.js
+++ script.js
@@ -32,3 +32,7 @@ function squareSum(numbers) {
     return n * n + sum;
   }, 0);
 }
+//A Needle in the Haystack
+function findNeedle(haystack) {
+  return "found the needle at position " + haystack.indexOf("needle");
+}
