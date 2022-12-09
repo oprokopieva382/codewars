e28a0dcf05dc57915842e8d53404a0a73dbae0a1
Oksana(KsyFate) <oprokopieva382@gmail.com>
2022-12-08 22:21:22 -0600

    Grasshopper - Personalized Message test

--- script2.js
+++ script2.js
@@ -2,3 +2,7 @@
 function paperwork(n, m) {
   return n < 0 || m < 0 ? 0 : n * m;
 }
+//Grasshopper - Personalized Message
+function greet(name, owner) {
+  return name === owner ? "Hello boss" : "Hello guest";
+}
