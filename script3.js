c48c3b73920cf2f75729b1748a31e07d988139f1
Oksana(KsyFate) <oprokopieva382@gmail.com>
2022-12-09 22:30:44 -0600

    Remove First and Last Character

--- script3.js
+++ script3.js
@@ -17,3 +17,7 @@ function getGrade(s1, s2, s3) {
   var s = (s1 + s2 + s3) / 3;
   return s >= 90 ? "A" : s >= 80 ? "B" : s >= 70 ? "C" : s >= 60 ? "D" : "F";
 }
+//Remove First and Last Character
+function removeChar(str) {
+  return str.split("").slice(1, -1).join("");
+}
