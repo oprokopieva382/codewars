56f1c8adf82859a9f4bbd0d026cbc94099105d8e
Oksana(KsyFate) <oprokopieva382@gmail.com>
2022-12-09 22:22:31 -0600

    faster option for gerGrade

--- script3.js
+++ script3.js
@@ -12,3 +12,8 @@ function getGrade(s1, s2, s3) {
     return "F";
   }
 }
+//or faster
+function getGrade(s1, s2, s3) {
+  var s = (s1 + s2 + s3) / 3;
+  return s >= 90 ? "A" : s >= 80 ? "B" : s >= 70 ? "C" : s >= 60 ? "D" : "F";
+}
